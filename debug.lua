8ec010b98ed91537f7ab9b75d226eab157ca4caf
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2019-11-24 00:30:36 +0900

    Test commit from Int-J

--- debug.lua
+++ debug.lua
@@ -7,10 +7,10 @@ This macro colors red and white
 function main ()
    -- currentSelection = Selection_GetAtoms();
     --Command_ShowRibbons(true);
-    total = List_Length(Selection_GetResidue());
+    --total = List_Length(Selection_GetResidue());
     --residues = Selection_GetResidues();
     --Selection_Clear();
-    return total;
+    return 1;
 --    for idx, residue in List_Pairs(residues) do
 --        Selection_Change("add", residue);
 --        if (idx < total/2) then
@@ -22,4 +22,5 @@ function main ()
 --    end;
 --    Selection_Clear();
 --    Selection_Change("add",currentSelection);
-end;
+end
+
