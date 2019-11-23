a72c13d33076a5e867d1e5e7e7eeb766a3601f6b
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2019-11-24 00:20:51 +0900

    Test commit from Int-J

--- debug.lua
+++ debug.lua
@@ -10,15 +10,16 @@ function main ()
     total = List_Length(Selection_GetResidue());
     residues = Selection_GetResidues();
     Selection_Clear();
-    for idx, residue in List_Pairs(residues) do
-        Selection_Change("add", residue);
-        if (idx < total/2) then
-            Command_ColoringRibbons("mono", Color_RGB(255, 255, 255));
-        else
-            Command_ColoringRibbons("mono", Color_RGB(255, 0, 0));
-        end;
-        Selection_Change("remove", residue);
-    end;
-    Selection_Clear();
-    Selection_Change("add",currentSelection);
+    return total;
+--    for idx, residue in List_Pairs(residues) do
+--        Selection_Change("add", residue);
+--        if (idx < total/2) then
+--            Command_ColoringRibbons("mono", Color_RGB(255, 255, 255));
+--        else
+--            Command_ColoringRibbons("mono", Color_RGB(255, 0, 0));
+--        end;
+--        Selection_Change("remove", residue);
+--    end;
+--    Selection_Clear();
+--    Selection_Change("add",currentSelection);
 end;
