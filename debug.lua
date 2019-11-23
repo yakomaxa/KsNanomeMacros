f94cdeaf2eae641f92d29e05dd49765a12e9d7eb
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2019-11-24 01:48:15 +0900

    Test commit from Int-J

--- debug.lua
+++ debug.lua
@@ -5,28 +5,21 @@ This macro colors red and white
 ]]
 
 function main ()
- --Selection_Clear();
- --residues=Search_GetResidues(" > A > ");
- --Selection_Change("add", chainA);
- --Selection_Change("remove", Search_GetResidues(">> HOH"));
- --total = List_Length(Selection_GetAtoms(chain));
- --residues = Chain_GetResidues(chain)
- --for residue in residues do
- --if Residue_GetName(residue) ~= "HOH" then
- --print("HOH")
- --else
- --Selection_Clear()
- --Selection_Change("add", residue)
- --count=count+1;
- --if (count < 30) then
- Selection_All();
+ Selection_Clear();
+ residues=Search_GetResidues(" > A > ");
+ residues = Chain_GetResidues(chain)
+ count=0
  Command_ShowRibbons(false);
- Command_ColoringRibbons("mono", Color_RGB(255, 255, 255));
- Command_ShowRibbons(true);
-  --   else
---      Command_ColoringRibbons("mono", Color_RGB(255, 0, 0));
---     end
---    end
---   end
-   --return total .. "residues"
+ for residue in residues do
+  Selection_Clear()
+  Selection_Change("add", residue)
+  count=count+1;
+  if (count < 30) then
+   Command_ColoringRibbons("mono", Color_RGB(255, 255, 255));
+  else
+   Command_ColoringRibbons("mono", Color_RGB(0, 0, 255));
+  end
+ end
+  Command_ShowRibbons(true);
+
 end
