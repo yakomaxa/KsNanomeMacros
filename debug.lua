e311f5166bb247949e8aa1b9dc447e43dccb7bce
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2019-11-24 01:19:09 +0900

    Test commit from Int-J

--- debug.lua
+++ debug.lua
@@ -11,13 +11,25 @@ function main ()
  Selection_Change("remove", Search_GetResidues(">> HOH"));
  total = List_Length(Selection_GetAtoms(chain));
  Selection_Clear();
- for idx,residue in List_Pairs(Chain_GetResidues(chain)) do
-  Selection_Change("add", residue);
-  if (idx < total/2) then
-   Command_ColoringRibbons("mono", Color_RGB(255, 255, 255));
-  else
-   Command_ColoringRibbons("mono", Color_RGB(255, 0, 0));
-  end;
+ for complex in List_Iterator(Workspace_GetComplexes()) do
+  molecule = Complex_GetCurrentMolecule(complex);
+  for chain in List_Iterator(Molecule_GetChains(molecule)) do
+   total = List_Length(Selection_GetAtoms(chain));
+   count=0
+   for residue in List_Iterator(Chain_GetResidues(chain)) do
+    if Residue_GetName(residue) ~= "HOH" then
+     print("HOH")
+    else
+     Selection_Clear();
+     Selection_Change("add", residue);
+     if (idx < total/2) then
+      Command_ColoringRibbons("mono", Color_RGB(255, 255, 255));
+     else
+      Command_ColoringRibbons("mono", Color_RGB(255, 0, 0));
+     end
+    end
+   end
+   return total .. "residues"
+  end
  end
- return total .. "residues"
 end
