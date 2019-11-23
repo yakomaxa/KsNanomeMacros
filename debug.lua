533ecd38c404614a191880666e16cb255fc05fa4
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2019-11-24 01:43:46 +0900

    Test commit from Int-J

--- debug.lua
+++ debug.lua
@@ -11,16 +11,18 @@ function main ()
  --Selection_Change("remove", Search_GetResidues(">> HOH"));
  --total = List_Length(Selection_GetAtoms(chain));
  --residues = Chain_GetResidues(chain)
- ---for residue in residues do
- ---if Residue_GetName(residue) ~= "HOH" then
+ --for residue in residues do
+ --if Residue_GetName(residue) ~= "HOH" then
  --print("HOH")
  --else
- ---Selection_Clear()
- ---Selection_Change("add", residue)
- ---count=count+1;
- ---if (count < 30) then
+ --Selection_Clear()
+ --Selection_Change("add", residue)
+ --count=count+1;
+ --if (count < 30) then
  Selection_All();
+ Command_ShowRibbons(false);
  Command_ColoringRibbons("mono", Color_RGB(255, 255, 255));
+ Command_ShowRibbons(true);
   --   else
 --      Command_ColoringRibbons("mono", Color_RGB(255, 0, 0));
 --     end
