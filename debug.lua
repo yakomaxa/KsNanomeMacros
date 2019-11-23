796baced965cfbd6ec9ac2c3eae9df345d198773
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2019-11-24 01:29:22 +0900

    Test commit from Int-J

--- debug.lua
+++ debug.lua
@@ -10,11 +10,10 @@ function main ()
  --Selection_Change("add", chainA);
  --Selection_Change("remove", Search_GetResidues(">> HOH"));
  --total = List_Length(Selection_GetAtoms(chain));
- Selection_Clear();
  for complex in List_Iterator(Workspace_GetComplexes()) do
   molecule = Complex_GetCurrentMolecule(complex);
   for chain in List_Iterator(Molecule_GetChains(molecule)) do
-   total = List_Length(Selection_GetAtoms(chain));
+   --total = List_Length(Selection_GetAtoms(chain));
    count=0
    for residue in List_Iterator(Chain_GetResidues(chain)) do
     if Residue_GetName(residue) ~= "HOH" then
@@ -23,7 +22,7 @@ function main ()
      Selection_Clear();
      Selection_Change("add", residue);
      count=count+1;
-     if (count < total/2) then
+     if (count < 30) then
       Command_ColoringRibbons("mono", Color_RGB(255, 255, 255));
      else
       Command_ColoringRibbons("mono", Color_RGB(255, 0, 0));
