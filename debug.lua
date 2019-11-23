eff51c7dfc95bb1eb0fe472aea80422336097123
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2019-11-24 01:11:20 +0900

    Test commit from Int-J

--- debug.lua
+++ debug.lua
@@ -9,7 +9,15 @@ function main ()
  chainA=Search_GetResidues(" > A > ");
  Selection_Change("add", chainA);
  Selection_Change("remove", Search_GetResidues(">> HOH"));
- --total = List_Length(Selection_GetAtoms());
- --return total .. "atoms"
- return "Getting tired"
+ total = List_Length(Selection_GetAtoms(chain));
+ Selection_Clear();
+ for idx,residue in List_Pairs(Chain_GetResidues(chain)) do
+  Selection_Change("add", residue);
+  if (idx < total/2) then
+   Command_ColoringRibbons("mono", Color_RGB(255, 255, 255));
+  else
+   Command_ColoringRibbons("mono", Color_RGB(255, 0, 0));
+  end;
+ end
+ return total .. "residues"
 end
