35119d1541de4c002b6aaafc8182c4be39beee9b
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2019-11-24 00:56:55 +0900

    Test commit from Int-J

--- debug.lua
+++ debug.lua
@@ -5,6 +5,10 @@ This macro colors red and white
 ]]
 
 function main ()
+ Selection_Clear();
+ chainA=Search_GetResidues(" > A > ");
+ Selection_Change("add", chainA);
+ Selection_Change("remove", Search_GetResidues(">> HOH"));
  total = List_Length(Selection_GetResidue());
  return total .. "residues"
 end
