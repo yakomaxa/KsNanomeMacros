9079b896c86564c8a525445240ce4d3733bc7c57
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2019-11-24 00:35:57 +0900

    Test commit from Int-J

--- debug.lua
+++ debug.lua
@@ -5,22 +5,6 @@ This macro colors red and white
 ]]
 
 function main ()
-    -- currentSelection = Selection_GetAtoms();
-    --Command_ShowRibbons(true);
-    --total = List_Length(Selection_GetResidue());
-    --residues = Selection_GetResidues();
-    --Selection_Clear();
-    return "yes";
---    for idx, residue in List_Pairs(residues) do
---        Selection_Change("add", residue);
---        if (idx < total/2) then
---            Command_ColoringRibbons("mono", Color_RGB(255, 255, 255));
---        else
---            Command_ColoringRibbons("mono", Color_RGB(255, 0, 0));
---        end;
---        Selection_Change("remove", residue);
---    end;
---    Selection_Clear();
---    Selection_Change("add",currentSelection);
+ return "YES"
 end
 
